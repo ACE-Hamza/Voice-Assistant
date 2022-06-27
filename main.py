5138ff6a343c1892756f55a1fc88914cde3b280e
Hamza <hrktv7hub@gmail.com>
2022-06-28 01:26:52 +0530

    add queries for wikipedia, youtube, and google

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import webbrowser
 
 engine = pyttsx3.init("sapi5")
 voices = engine.getProperty('voices')
@@ -55,4 +56,12 @@ if __name__ == '__main__':
             speak("According to Wikipedia")
             print(results)
             speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        
 
